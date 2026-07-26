723945fbd4366beb8df69259d151f3d740119229
AbbasiDaniel <danielabbasi2010@gmail.com>
2026-07-27 00:26:48 +0400

    Update manage_test.py

--- manage_test.py
+++ manage_test.py
@@ -71,13 +71,14 @@ def get_driver():
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     options.add_argument("--blink-settings=imagesEnabled=false")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--no-first-run")
     options.add_argument("--no-default-browser-check")
     options.add_argument("--disable-infobars")
     options.add_argument("--disable-extensions")
-
+    options.add_argument("--single-process")
     user_agents = [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/148.0.7559.60 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/147.0.7499.193 Safari/537.36",
