6afd6ef0efeebf1ac33cf2575eda87fef1a4d856
AbbasiDaniel <danielabbasi2010@gmail.com>
2026-07-26 23:08:27 +0400

    Update manage_test.py

--- manage_test.py
+++ manage_test.py
@@ -127,11 +127,12 @@ def get_price_with_selenium(url, wait_seconds=120):
         driver.get(url)
         time.sleep(30)
         print("Page Title is:", driver.title)
+        print(driver.page_source[:2000], flush=True)
         spans = driver.find_elements(By.CSS_SELECTOR, "span.ux-textspans")
         for span in spans:
-            print(2)
+            #print(2)
             text = span.text.strip()
-            print(text)
+            #print(text)
             num=""
             if text.startswith("$") or text.startswith("U"):
                 for c in text:
