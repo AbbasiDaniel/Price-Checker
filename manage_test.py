210adb8adad76972e3fc4d3006503ed4c28be493
AbbasiDaniel <danielabbasi2010@gmail.com>
2026-07-26 22:50:54 +0400

    Update manage_test.py

--- manage_test.py
+++ manage_test.py
@@ -118,10 +118,11 @@ def get_driver():
 def get_price_with_selenium(url, wait_seconds=120):
     print("************************************************", flush=True)
     try:
+
+        driver = get_driver()
+        print("daram miam bala saret")
         driver.get("https://www.ebay.com")
         time.sleep(15)
-        print("daram miam bala saret")
-        driver = get_driver()
         print(">>> BEFORE TRY BLOCK <<<", flush=True)
         driver.get(url)
         time.sleep(30)
