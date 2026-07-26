5b9064e82948533fbdbd45bf0674d618b0b885b1
AbbasiDaniel <danielabbasi2010@gmail.com>
2026-07-26 20:44:39 +0400

    Update manage_test.py

--- manage_test.py
+++ manage_test.py
@@ -73,7 +73,9 @@ def get_driver():
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/147.0.7499.193 Safari/537.36",
     ]
     options.add_argument(f"user-agent={random.choice(user_agents)}")
+    print("eeeee")
     driver = uc.Chrome(options=options)
+    print("retete")
     driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
         "source": """
         Object.defineProperty(navigator, 'webdriver', {get: () => undefined});
