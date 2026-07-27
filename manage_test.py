b6be5c3c85340b98949d4f6d2fcc02c1d8721135
AbbasiDaniel <danielabbasi2010@gmail.com>
2026-07-27 23:41:41 +0400

    Update manage_test.py

--- manage_test.py
+++ manage_test.py
@@ -128,7 +128,7 @@ chrome.webRequest.onAuthRequired.addListener(
 
 def get_driver():
     options = uc.ChromeOptions()
-    
+    options.add_argument("--headless=new")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
